2eb0d8fbbe471d92b49ecee9692283fa4bea4ad4
Tailor Lopez <tailor.alm@gmail.com>
2024-03-15 14:12:16 +0000

    lint and remove package-lock.json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config';
 import {Telegraf, Context} from 'telegraf';
-import PhotoMessageController from "./router/photo-message.controller";
-import VoiceMessageController from "./router/voice-message.controller";
-import TextMessageController from "./router/text-message.controller";
-import CommandMessageController from "./router/command-message.controller";
-import DocumentMessageController from "./router/document-message.controller";
+import PhotoMessageController from './router/photo-message.controller';
+import VoiceMessageController from './router/voice-message.controller';
+import TextMessageController from './router/text-message.controller';
+import CommandMessageController from './router/command-message.controller';
+import DocumentMessageController from './router/document-message.controller';
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
 bot.use(checkAuthorization);
@@ -18,7 +18,7 @@ TextMessageController.router(bot);
 bot.launch();
 
 function checkAuthorization(ctx: Context, next: () => any) {
-  if(!process.env.MY_TELEGRAM_ID) ctx.reply(`Your chat ID: ${ctx.from?.id}`);
+  if (!process.env.MY_TELEGRAM_ID) ctx.reply(`Your chat ID: ${ctx.from?.id}`);
   if (ctx.from?.id !== Number(process.env.MY_TELEGRAM_ID)) {
     return ctx.reply('Not Authorized');
   }
