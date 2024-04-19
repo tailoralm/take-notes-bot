5710f2df81f8b26e5aeedc6e20b1e6beb753f18d
Tailor Lopez <tailor.alm@gmail.com>
2024-04-19 16:00:54 +0100

    first commit with ideas

---
+++ src/shared/services/ai-services/analyze-text.service.ts
@@ -0,0 +1,22 @@
+/*
+  Extract tags from the text
+  Create a title for the text
+  Create a subtitle for the text
+  Create a description for the text
+  Create a text reorganized and formatted by the AI
+  Create a list of keywords for the text
+  Create a list of entities for the text
+  Create a list of categories for the text
+
+{
+title: string,
+subtitle: string,
+description: string,
+tags: string[]
+originalText: string,
+aiProcessedText: string,
+keywords: string[],
+entities: string[],
+categories: string[],
+}
+ */
---
+++ src/shared/services/ai-services/voice-to-text.service.ts
@@ -0,0 +1,5 @@
+// Receive the audio file and process it with the Speech-to-Text API
+
+// in the original controller that calls the service, add:
+// Save the transcription in a file with the same name as voice file in processed-voices folder
+// Move voice file to the processed-voices folder
