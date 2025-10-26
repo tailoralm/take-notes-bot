a4f718ba674881abede537d306013fe4bfc3ee8a
Tailor <tailor.alm@gmail.com>
2025-10-26 19:06:52 +0000

    fix dotenv and run

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { EFolders } from './src/actions/folders.enum';
 import Router from './src/actions/router';
 import { ensureDirectoryExistence } from './src/utils/general-utils';
 
-export function run() {
-  if (!process.env.DATA_DIR) {
-    throw new Error('DATA_DIR environment variable is not set');
-  }
-  // Create all folders
-
-  Object.values(EFolders).forEach((folder: string) => ensureDirectoryExistence(folder + '/any.jpg'));
-
-  new Router();
+if (!process.env.DATA_DIR) {
+  throw new Error('DATA_DIR environment variable is not set');
 }
+// Create all folders
+
+Object.values(EFolders).forEach((folder: string) => ensureDirectoryExistence(folder + '/any.jpg'));
+
+new Router();
